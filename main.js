f0a4174481e625acff0564212109aabe9f472661
avz <arepo@nologin.ru>
2014-01-18 16:23:11 +0400

    JSDoc fixes

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ exports.gettime = binding.gettime;
 
 /**
  * The function finds the resolution (precision) of the
- * specified clock clockId and stores it in the struct timespec.
+ * specified clock clockId.
  * The resolution of clocks depends on the implementation and cannot be
  * configured by a particular process.
  * @param {Integer} clockId Clock identifier, specified by constant
@@ -38,21 +38,21 @@ exports.MONOTONIC = binding.MONOTONIC;
 
 /**
  * @warning since Linux 2.6.32; Linux-specific
- * A faster but less precise version of CLOCK_REALTIME.
+ * A faster but less precise version of REALTIME.
  * Use when you need very fast, but not fine-grained timestamps.
  */
 exports.REALTIME_COARSE = binding.REALTIME_COARSE;
 
 /**
  * @warning since Linux 2.6.32; Linux-specific
- * A faster but less precise version of CLOCK_MONOTONIC.
+ * A faster but less precise version of MONOTONIC.
  * Use when you need very fast, but not fine-grained timestamps.
  */
 exports.MONOTONIC_COARSE = binding.MONOTONIC_COARSE;
 
 /**
  * @warning since Linux 2.6.28; Linux-specific
- * Similar to CLOCK_MONOTONIC, but provides access to a raw hardware-based time
+ * Similar to MONOTONIC, but provides access to a raw hardware-based time
  * that is not subject to NTP adjustments or the incremental adjustments
  * performed by adjtime(3).
  */
@@ -60,10 +60,10 @@ exports.MONOTONIC_RAW = binding.MONOTONIC_RAW;
 
 /**
  * @warning since Linux 2.6.39; Linux-specific
- * Identical to CLOCK_MONOTONIC, except it also includes
+ * Identical to MONOTONIC, except it also includes
  * any time that the system is suspended.  This allows applications to get
  * a suspend-aware monotonic clock without having to deal with
- * the complications of CLOCK_REALTIME, which may have discontinuities
+ * the complications of REALTIME, which may have discontinuities
  * if the time is changed using settimeofday(2).
  */
 exports.BOOTTIME = binding.BOOTTIME;
